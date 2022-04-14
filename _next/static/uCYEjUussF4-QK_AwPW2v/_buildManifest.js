09298ebb7b572524e8466422d52b5aa1b1ed9df1
GitHub Action <tkw1536@users.noreply.github.com>
2022-04-14 12:43:58 +0000

    40833feae724eea24df2a4811552e25d8fb377a8

---
+++ _next/static/uCYEjUussF4-QK_AwPW2v/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/f24c28eba46d0b5a.css","static/chunks/pages/index-e425aec99cdc3fb8.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
